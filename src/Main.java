086e160caaf2e5d8bc5d9f9ff3cb0aa2443013dd
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-02-28 21:08:47 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = 1.87;
+        double weight = 98;
+        int bmi = service.calculate(98, 1.87);
+        System.out.println( " Индекс массы тела = " + bmi );
+    }
+}
